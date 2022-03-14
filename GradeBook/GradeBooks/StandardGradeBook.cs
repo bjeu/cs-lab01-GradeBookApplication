72c61c1092e30e6063f2c2bb795d851337277c69
bjeu <pawel.trojanski@microsoft.wsei.edu.pl>
2022-03-14 08:26:49 +0100

    RankedGradeBook class created

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,7 +8,6 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Name = name;
             Type = Enums.GradeBookType.Standard;
         }
     }
